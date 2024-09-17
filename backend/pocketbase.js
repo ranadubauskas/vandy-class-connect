aae051f3178f24c96d98de6000538ad944c42630
Rana Dubauskas <ranadubauskas@Ranas-MacBook-Pro.local>
2024-09-17 09:21:11 -0500

    Setup

---
+++ backend/pocketbase.js
@@ -0,0 +1,4 @@
+import PocketBase from 'pocketbase';
+
+const url = "https://vandy-class-connect.pockethost.io/";
+const client = new PocketBase(url)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
+import React, { useEffect } from 'react';
+import { client } from '../../pocketbase.js';
 import './App.css';
 
 function App() {
+  useEffect(()=> {
+    client.collection("users").getFullList().then(res=> console.log(res));
+  })
   return (
     <div className="App">
       <header className="App-header">
